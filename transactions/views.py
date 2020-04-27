8a7f77091e63976996b978914d3d6cb48c4ceeb7
Sloshpit <neel.maheshwari@gmail.com>
2020-04-27 15:49:33 -0400

    updated transaction view

--- transactions/views.py
+++ transactions/views.py
@@ -9,20 +9,6 @@ from django.views.generic.list import ListView
 from .forms import CreateTransactionForm
 from django.shortcuts import render, redirect, reverse
 
-def index(request):
-    show_transactions = Transaction.objects.filter(trans_date__range=["2020-04-01", "2020-04-30"])
-
-    template = loader.get_template('transactions/index.html')
-    total = Transaction.objects.filter(trans_date__range=["2020-04-01", "2020-04-30"]).aggregate(sum=Sum('amount'))['sum'] or 0.00
-    total = "{:.2f}".format(total)
-    print (show_transactions)
-    context = {
-        'show_transactions':show_transactions,
-        'total': total,
-    }
-    return HttpResponse(template.render(context, request))
-
-
 
 class TransactionCreate (CreateView):
 
@@ -34,11 +20,11 @@ class TransactionCreate (CreateView):
 
 class TransactionUpdate (UpdateView):
     template_name = 'transactions/transaction_form.html'
-    #form_class = CreateTransactionForm()
     form = CreateTransactionForm
     success_url = reverse_lazy ('transaction-index')
     model = Transaction
     fields = ['store', 'description', 'amount','trans_date', 'category', 'account_name']
+
 class TransactionDelete (DeleteView):
     model = Transaction
     template_name = 'transactions/transaction_delete.html'
