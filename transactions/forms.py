b5ae905c2b870318e9ebc4b7ffad149491a7c48b
Sloshpit <neel.maheshwari@gmail.com>
2020-05-06 09:10:28 -0400

    Transaction updates
    Fixed the create, update and delete logic for past transactions

--- transactions/forms.py
+++ transactions/forms.py
@@ -7,10 +7,86 @@ from categories.models import Category
 from .models import Transaction
 
 class CreateTransactionForm(forms.ModelForm):
-
+    CHOICES = [('spend', 'Spend'), ('income', 'Income/Refund')]
+    transaction_type = forms.ChoiceField(
+        choices = CHOICES, # this is optional
+        widget = forms.RadioSelect,
+        initial ='spend'
+    )
     class Meta:
         model = Transaction
-        fields ='__all__'
+        fields =('store','description','transaction_type','category','amount', 'trans_date','account_name')
         widgets = {
             'trans_date': DatePickerInput(), # default date-format %m/%d/%Y will be used
         }
+
+    def clean_amount(self):
+
+       for field, value in self.cleaned_data.items():
+           if self.cleaned_data['transaction_type']=='spend':
+               amount = self.cleaned_data['amount'] *-1
+       return amount
+
+    
+    def clean_trans_date(self):
+        trans_date = self.cleaned_data['trans_date']
+        return trans_date
+        
+    def clean_description(self):
+        description =  self.cleaned_data ['description']
+        return description
+
+    def clean_store(self):
+        store = self.cleaned_data['store']
+        return store
+
+    def clean_account_name(self):
+        account_name = self.cleaned_data ['account_name']
+        return (account_name)
+
+    def clean_category(self):
+        category = self.cleaned_data ['category']
+        return (category)
+
+class UpdateTransactionForm(forms.ModelForm):
+    CHOICES = [('spend', 'Spend'), ('income', 'Income/Refund')]
+    transaction_type = forms.ChoiceField(
+        choices = CHOICES, # this is optional
+        widget = forms.RadioSelect,
+        initial ='spend'
+    )
+    class Meta:
+        model = Transaction
+        fields =('account_name','store','description','category','transaction_type','amount', 'trans_date')
+        widgets = {
+            'trans_date': DatePickerInput(), # default date-format %m/%d/%Y will be used
+        }
+
+    def clean_amount(self):
+
+       for field, value in self.cleaned_data.items():
+           if self.cleaned_data['transaction_type']=='spend':
+               amount = self.cleaned_data['amount'] *-1
+       return amount
+
+    
+    def clean_trans_date(self):
+        trans_date = self.cleaned_data['trans_date']
+        return trans_date
+        
+    def clean_description(self):
+        description =  self.cleaned_data ['description']
+        return description
+
+    def clean_store(self):
+        store = self.cleaned_data['store']
+        return store
+
+    def clean_account_name(self):
+        account_name = self.cleaned_data ['account_name']
+        return (account_name)
+
+    def clean_category(self):
+        category = self.cleaned_data ['category']
+        return (category)
+        
--- transactions/views.py
+++ transactions/views.py
@@ -7,7 +7,7 @@ from accounts.models import Account,AccountBalance
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
-from .forms import CreateTransactionForm
+from .forms import CreateTransactionForm, UpdateTransactionForm
 from django.shortcuts import render, redirect, reverse
 from datetime import datetime
 from calendar import monthrange
@@ -47,25 +47,45 @@ class TransactionCreate (CreateView):
      form_class = CreateTransactionForm
      success_url = reverse_lazy('transaction-index') 
      model = Transaction
+
      def form_valid(self, form):
-        trans_date = form.cleaned_data['trans_date']
-        amount = form.cleaned_data['amount']
-        description = form.cleaned_data ['description']
         store = form.cleaned_data ['store']
-        acct_name = form.cleaned_data ['account_name']
         category = form.cleaned_data ['category']
+        acct_name = form.cleaned_data['account_name']
+        amount = form.cleaned_data['amount']
+        trans_date = form.cleaned_data['trans_date']
+        print (type(trans_date))
         self.object = form.save()
-
-        balance_description = str(store) +" "+ str(category)
-        #last_account_for_account_name = AccountBalance.objects.filter(account_name=acct_name).last()
-        latest_account = AccountBalance.objects.filter(account__account_name=acct_name).values('account__account_name', 'balance', 'balance_date').latest('balance_date')
-
-        account_balance=latest_account['balance']
-        new_account_balance=amount + float(account_balance)
-        print (new_account_balance)
         now = datetime.today()
-        new_record = AccountBalance(account=acct_name, balance_description = balance_description, balance=new_account_balance, balance_date=now)
-        new_record.save()
+        print (type(now))
+        balance_description = str(store) +" "+ str(category)
+
+        if now.date() == trans_date.date():
+        #if record is equal to today
+        #last_account_for_account_name = AccountBalance.objects.filter(account_name=acct_name).last()
+            latest_account = AccountBalance.objects.filter(account__account_name=acct_name).values('account__account_name', 'balance', 'balance_date').latest('balance_date')
+            account_balance=latest_account['balance']
+            new_account_balance=amount + float(account_balance)        
+            new_record = AccountBalance(account=acct_name, balance_description = balance_description, balance=new_account_balance, balance_date=trans_date)
+            new_record.save()
+        else:
+           #the transaction is in the past so you need to add a new balance and update all the other balances
+
+
+           print ('from here calculate a new_record ')
+           records_to_update = AccountBalance.objects.filter(balance_date__gte=trans_date, balance_date__lte = now)
+           print (records_to_update)
+
+           for record in records_to_update:
+                record.balance = record.balance + amount
+                record.save()   
+           latest_account = AccountBalance.objects.filter(balance_date__lt=trans_date).order_by("-balance_date")[0]
+           print ('------in else---got the latest record in the past')
+           print (latest_account)
+           account_balance=latest_account.balance
+           new_account_balance=amount + float(account_balance)        
+           new_record = AccountBalance(account=acct_name, balance_description = balance_description, balance=new_account_balance, balance_date=trans_date)
+           new_record.save() 
        
         return super().form_valid(form)
 
@@ -73,22 +93,63 @@ class TransactionCreate (CreateView):
 
 class TransactionUpdate (UpdateView):
     template_name = 'transactions/transaction_form.html'
-    form_class = CreateTransactionForm
+    form_class = UpdateTransactionForm
     success_url = reverse_lazy('transaction-index') 
     #form = CreateTransactionForm
     #success_url = reverse_lazy ('transaction-index')
     model = Transaction
     #fields = ['store', 'description', 'amount','trans_date', 'category', 'account_name']
+    #get the date of the transaction
+    #get all the transactions from that date to present date (today)
+    #perform calculation on the updated balance for each
+    #write the balance to the db, along with any other changes.
 
+    def form_valid(self, form):
+        self.object = self.get_object()
+        today = datetime.today()
+        store = form.cleaned_data ['store']
+        category = form.cleaned_data ['category']
+        acct_name = form.cleaned_data['account_name']
+        amount = form.cleaned_data['amount']
+        print ('-------get-object')
+        print (self.get_object())
+        print (self.object.amount)
+        print ('-----------------')
+        amount_difference = amount - self.object.amount
+        print ('difference:')
+        print (amount_difference)
+        trans_date = form.cleaned_data['trans_date']
+        balance_records = AccountBalance.objects.filter(account__account_name=acct_name, balance_date__range = [trans_date, today]  )
+        for record in balance_records:
+            record.balance = record.balance + amount_difference
+            record.save()
+        return super().form_valid(form)
 
 class TransactionDelete (DeleteView):
     model = Transaction
     form_class = CreateTransactionForm
     #success_url = reverse_lazy('transaction-index') 
     template_name = 'transactions/transaction_delete.html'
-    context_object_name = 'transaction'
+#    context_object_name = 'transaction'
     success_url = reverse_lazy('transaction-index')
     #fields ='__all__'
+
+
+    def delete(self, *args, **kwargs):
+        today = datetime.today()
+        self.object = self.get_object()
+        print (self.object)
+        amount = self.object.amount
+        trans_date = self.object.trans_date
+        acct_name = self.object.account_name
+        account_record_to_delete = AccountBalance.objects.filter(balance_date=trans_date, account=acct_name).delete()
+        records_to_update = AccountBalance.objects.filter(balance_date__gte=trans_date, balance_date__lte = today)
+
+        for record in records_to_update:
+            record.balance = record.balance - amount
+            record.save()
+        
+        return super(TransactionDelete, self).delete(*args, **kwargs)
 
 class TransactionList (ListView): 
     template_name = 'transactions/index.html'
